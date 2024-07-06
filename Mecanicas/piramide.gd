98d1e9afb33f417dfa450e85e4de28fe9aab7626
MartinCinco <jet5boymartin@gmail.com>
2024-07-05 20:16:00 -0900

    Update piramide.gd

--- Mecanicas/piramide.gd
+++ Mecanicas/piramide.gd
@@ -3,6 +3,8 @@ extends Interactable
 
 var dialog_system = preload("res://Mecanicas/text_box.tscn").instantiate()
 
+#comentario de prueba, washalo y me dices
+
 func _on_interacted(body):
 	
 	dialog_system.hablar("ESTO ES UN DIALOGO")
